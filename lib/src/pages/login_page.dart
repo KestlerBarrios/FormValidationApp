31acca95894c752f46560b70b78d69d7872cb37b
Kestler Barrios <luiseduardo3213@gmail.com>
2021-07-09 17:00:19 -0600

    UI terminada

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -7,6 +7,7 @@ class LoginPage extends StatelessWidget {
       body: Stack(
         children: [
           _crearFondo(context),
+          _loginForm(context),
         ],
       ),
     );
@@ -62,6 +63,100 @@ class LoginPage extends StatelessWidget {
           ),
         )
       ],
+    );
+  }
+
+  Widget _loginForm(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SafeArea(
+              child: Container(
+            height: 180,
+          )),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 50.0),
+            margin: EdgeInsets.symmetric(vertical: 30.0),
+            width: size.width * 0.85,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 3.0,
+                  offset: Offset(0.0, 5.0),
+                  spreadRadius: 3.0,
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'Ingreso',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                SizedBox(height: 40.0),
+                _crearEmail(),
+                _crearPassword(),
+                _crearBoton()
+              ],
+            ),
+          ),
+          Text('¿Olvido la contraseña?'),
+          SizedBox(
+            height: 100.0,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _crearEmail() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            icon: Icon(
+              Icons.alternate_email,
+              color: Colors.deepPurple,
+            ),
+            labelText: 'Correo Electronico',
+            hintText: 'ejemplo@correo.com'),
+      ),
+    );
+  }
+
+  Widget _crearPassword() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: TextField(
+        obscureText: true,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(
+            Icons.lock_outline,
+            color: Colors.deepPurple,
+          ),
+          labelText: 'Contraseña',
+        ),
+      ),
+    );
+  }
+
+  Widget _crearBoton() {
+    return RaisedButton(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+        child: Text('Ingresar'),
+      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+      color: Colors.deepPurple,
+      textColor: Colors.white,
+      onPressed: () {},
     );
   }
 }
